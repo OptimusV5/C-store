4c7badfa7d81b1865d5736c22bb57e6c2b245525
OptimusV5 <627133398@qq.com>
2014-05-03 22:21:21 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,42 @@
-#include "load.h"
 #include <iostream>
+#include "string.h"
+#include <stdio.h>
+#include "load.h"
+#include "query.h"
 #include "fileException.h"
-#include "fileOpenException.h" 
-int main() {
-    try {
-        load *l = new load();
-    } catch (FileOpenException& f) {
-        f.print();
+#include "fileOpenException.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if(strcmp(argv[1],"load") == 0 && argvc == 3) {
+        if(strcmp(argv[2],"orders") == 0) {
+            try {
+                load *l = new load();
+            } catch (FileException& f) {
+                f.print();
+            }
+        } else{
+            printf("Only orders can be loaded!\n");
+        }
+
+
     }
+    else if(strncmp(argv[1],"retrieve") == 0 && argvc == 4) {
+        if(strcmp(argv[2],"orders") == 0) {
+            int orderkey_;
+            sscanf(argv[4],"%d",&orderkey_);
+            try{
+                query *q = new query(orderkey_);
+            } catch (FileException) {
+                f.print();
+            }
+        } else{
+            printf("Only orders can be queried!\n");
+        }
+        }
+    else {
+        printf("Illegal command!\n");
+        }
     return 0;
 }
